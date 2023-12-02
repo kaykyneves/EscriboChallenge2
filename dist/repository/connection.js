a49952abbd7c28d48cbd6887416739b36e560944
kayky <kaykyneves42@gmail.com>
2023-12-02 00:17:52 -0300

    pushing to server

--- dist/repository/connection.js
+++ dist/repository/connection.js
@@ -2,11 +2,11 @@
 
 async function connect() {
     const connection = await _promise2.default.createConnection({
-        host: 'localhost',
-        port: 3307,
-        user: 'root',
-        password: '',
-        database: 'escribo',
+        host: 'bftv7ounevqtknuwfjdf-mysql.services.clever-cloud.com',
+        port: 3306,
+        user: 'utvwatelhpryloln',
+        password: '54DlkkRuVmkBmlbsG4ap',
+        database: 'bftv7ounevqtknuwfjdf',
     });
 
     return connection;
